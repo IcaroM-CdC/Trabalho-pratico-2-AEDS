512adce7887b1424207af67c58f45b0ba4a65583
Icaro <icaro.moreira@protonmail.com>
2021-04-18 23:20:29 -0300

    pequenos ajustes 2

--- FNC_3_SAT/FNC_3_SAT.c
+++ FNC_3_SAT/FNC_3_SAT.c
@@ -10,10 +10,16 @@ clause *read_clauses(int num_clauses, int num_literals){
 
     clause *clauses = (clause*) malloc(num_clauses * sizeof(clause));
 
-    // LÊ CADA CLAUSULA
+    /* 
+    
+        Para cada iteração de i, uma clausula nova é lida
+        e para cada iteração de j, uma das 3 tuplas da cláusula é lida
+    
+    */
+
     for (int i = 0; i < num_clauses; i++){
 
-        // LE CADA ELEMENTO DA CLAUSULA
+        
         printf("::::::::::::::::: CLAUSULA %d :::::::::::::::::\n", i + 1);
 
         for (int j = 0; j < CONST_ELEMENTS_CLAUSE; j++){
@@ -31,7 +37,7 @@ clause *read_clauses(int num_clauses, int num_literals){
 
     }
 
-    return clauses; //RETORNA VETOR DE CLAUSULAS
+    return clauses; /* Retorna o vetor de cláusulas */
 
 }
 
@@ -39,15 +45,24 @@ clause *read_clauses_automatic(int num_clauses, int num_literals, int **matrix){
 
     clause *clauses = (clause*) malloc(num_clauses * sizeof(clause));
 
-    // LÊ CADA CLAUSULA
+    
+    /* 
+    
+        Aassim como na função análoga para cada iteração de index1, uma clausula nova é lida
+        e para cada iteração de index2, uma das 3 tuplas da cláusula é lida
+    
+    */
+
     for (int index1 = 0; index1 < num_clauses; index1++){
 
-        // LE CADA ELEMENTO DA CLAUSULA
         for (int index2 = 0; index2 < num_literals; index2++){
 
-            //printf("%d" ,matrix[index1][index2]);
-
             if (matrix[index1][index2] != 0){
+
+                /* 
+                    Recebe o estado da cláusula a partir da posição da matriz 
+                    e recebe o indice do literal de acordo com o indice de index2
+                */
 
                 clauses[index1].tuples[index2].num_literal = index2;
                 clauses[index1].tuples[index2].state = matrix[index1][index2];
@@ -56,7 +71,7 @@ clause *read_clauses_automatic(int num_clauses, int num_literals, int **matrix){
         }
     }
 
-    return clauses; //RETORNA VETOR DE CLAUSULAS
+    return clauses; /* Retorna o vetor de cláusulas */
 
 }
 
@@ -64,12 +79,10 @@ clause *read_clauses_automatic(int num_clauses, int num_literals, int **matrix){
 int calculate_trues(clause *clauses, int num_clauses, int num_literals){
     
     
-
     int print_label = 1;
     long long int LINES = pow(2, num_literals);
     long long int result;
 
-    //int *line_result = (int*) malloc(sizeof(int) * num_literals);
     int line_result[100];
 
     
@@ -88,7 +101,11 @@ int calculate_trues(clause *clauses, int num_clauses, int num_literals){
 
             int clause_value = 0;
 
+            /* Para cada tupla da cláusula o for realiza a comparação */
+
             for (int k = 0; k < CONST_ELEMENTS_CLAUSE; k++){
+
+                /* Caso estiver negado etra no if, do contrario entra no else */
 
                 if (clauses[j].tuples[k].state == 1){
 
@@ -103,7 +120,7 @@ int calculate_trues(clause *clauses, int num_clauses, int num_literals){
                 }
             }
 
-            if (!clause_value){
+            if (!clause_value){     /* Caso ocorra um erro */
 
                 break;
 
@@ -134,6 +151,8 @@ int calculate_trues(clause *clauses, int num_clauses, int num_literals){
             }
 
             print_label = 0;
+
+            /* Formata a saida imprimindo F se o valor for 0 e V caso for 1 */
 
             for (int p = 0; p < num_literals; p++){
 
@@ -167,28 +186,23 @@ int calculate_trues(clause *clauses, int num_clauses, int num_literals){
 }
 
 int interactive(){
-    //------------------------DEF DADOS--------------------------
     
     int num_clauses;
     int num_variable;
     
     clause *clauses;
 
-    //------------------------LER DADOS--------------------------
+
     printf("\nNumero de clausulas: ");
     scanf("%d", &num_clauses);
 
     printf("Numero de variaveis: ");
     scanf("%d", &num_variable);
 
-    //------------------------MONTA TABELA
-    //int **matrix = generate_table(num_variable, 0);
     
-    //---------------------LER CLAUSULAS-------------------------
-    clauses = read_clauses(num_clauses, num_variable);
+    clauses = read_clauses(num_clauses, num_variable); /* Faz a leitura das cláusulas */
 
-    //---------------------CALCULA AS QUE TORNAM VERDADEIRAS E PRINTA
-    calculate_trues(clauses, num_clauses, num_variable);
+    calculate_trues(clauses, num_clauses, num_variable); /* Calcula os que satisfazem a equação booleana */
 
     return 1;
 
@@ -246,13 +260,13 @@ int automatic(int num_literals){
 
             while (matrix[index1][random] != 0){
 
-                random = rand() % (num_literals - 1);
+                random = rand() % (num_literals - 1);   /* Recebe um numero aleatório entre 0 e o numero de literais */
 
             }
 
             if (matrix[index1][random] == 0){
 
-                matrix[index1][random] = 1 + rand() % 2;
+                matrix[index1][random] = 1 + rand() % 2;    /* Recebe um numero aleatório entre 1 e 2 */
 
             }
         }
